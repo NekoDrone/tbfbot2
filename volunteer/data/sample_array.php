f95bd4dc8d828c2540df83586001c7b7cd4856f0
Sylfr <sylfr.serenity.tan@gmail.com>
2023-07-25 20:50:44 +0800

    Added php dev files. Added json parser.

---
+++ volunteer/data/sample_array.php
@@ -0,0 +1,34 @@
+Array
+(
+    [submission_id] => 5659941893914640056
+    [formID] => 232028672145049
+    [ip] => 118.200.134.193
+    [email] => sylfr.serenity.tan@gmail.com
+    [name] => Array
+        (
+            [first] => Sylfr
+            [last] => Tan
+        )
+
+    [age] => 24
+    [sexualorientation] => Queer
+    [gender] => Array
+        (
+            [0] => Male-to-Female/Trans Woman/Transfeminine
+            [1] => Non-binary
+            [other] => testinggg
+        )
+
+    [pronouns] => Array
+        (
+            [0] => She/her
+            [1] => They/them
+            [other] => testinggg
+        )
+
+    [telegramhandle] => @SylfrTan
+    [casedetails] => Testing! This is a mock.
+    [specialrequest] => test
+    [groupsessionbool] => Yes
+    [mailinglistbool] => Yes
+)
