d208179a115569cc246055b26909f0382521b8d7
Sylfr Serenity Tan <98162476+NekoDrone@users.noreply.github.com>
2023-07-26 16:43:55 +0800

    newCaseHello implemented. Rejects on existing case

--- index.ts
+++ index.ts
@@ -1,69 +1,58 @@
-import * as functions from "@google-cloud/functions-framework";
-import * as firestore from "@google-cloud/firestore";
+import * as functions from '@google-cloud/functions-framework';
+import * as firestore from '@google-cloud/firestore';
+import axios from 'axios';
 
-const axios = require('axios')
-const projectID = 'tbfcms2';
-const TELEGRAM_URL = process.env.TELEGRAM_URL ?? "";
-const telegramFullURL = TELEGRAM_URL + (process.env.TELEGRAM_BOT_KEY ?? "") + "/sendMessage";
-const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 const PROJ_ID = process.env.PROJ_ID;
 const COLLECTION_NAME: string = process.env.COLLECTION_NAME ?? 'undefined';
-const firestoreObject = new firestore.Firestore({
-  projectId: PROJ_ID
-});
+const firestoreCollection = new firestore.Firestore({// put up here because of how functions are instantiated. improves response times.
+    projectId: PROJ_ID
+}).collection(COLLECTION_NAME);
+const TELEGRAM_URL = process.env.TELEGRAM_URL ?? "";
+const telegramSendURL = TELEGRAM_URL + (process.env.TELEGRAM_BOT_KEY ?? "") + "/sendMessage";
+const NEW_CASE_FORM_URL = process.env.NEW_CASE_FORM_URL;
 
-functions.http('postJsonToFirestore', (req: functions.Request, res: functions.Response) => {
-
-  const jsonData = req.body;
-  console.log();
-  console.log("Incoming stream received at " + Date.now());
-  console.log("Request stream: " + JSON.stringify(jsonData));
-
-  var newCasePronouns = "";
-  for(const prns of jsonData.pronouns) {
-    newCasePronouns += prns;
-    newCasePronouns += ", ";
-  }
-
-  var newCaseIdentity = "";
-  for(const gender of jsonData.gender) {
-    newCaseIdentity += gender;
-    newCaseIdentity += ", ";
-  }
-
-  const teleMessage = "Hello! A new case has been received. Please check your preferred platform for more details.\n\n A shortened version of these details is provided below.\n\nTelegram Handle: " + jsonData.telegramhandle + "\nAge: " + jsonData.age + "\nPronouns: " + newCasePronouns + "\nGender Identity: " + newCaseIdentity;
-  const options = {
-    chat_id: TELEGRAM_CHAT_ID,
-    text: teleMessage
+functions.http('newCaseHello', async (req: functions.Request, res: functions.Response) => {
+    const updateObject = req.body;
+    const teleUser = '@' + updateObject.message.chat.username;
+    const teleUserId = updateObject.message.chat.id;
+    if(await userExistsinFirestore(teleUser)) {
+      console.log("User found! Sending rejection.");
+      sendAlreadyRegisteredMessage(teleUserId);
+      res.sendStatus(200);
+    }
+    else {
+      console.log("User not found. Sending form.");
+      sendNewRegistrationMessage(teleUserId, teleUser);
+      res.sendStatus(200);
     }
 
-  console.info("Trying to send telegram message at: " + telegramFullURL);
-  console.info("data = " + options);
-  
-  axios.post(telegramFullURL, options)
-  .then(function (response: any) {
-    console.log(response);
-    res.send({ status: 'OK'});
-  })
-  .catch(function (error: any) {
-    console.error(error);
-    res.sendStatus(500);
-  });
+})
 
-  firestoreObject.collection(COLLECTION_NAME).add(jsonData)
-    .then(doc => {
-      console.info('Stored new case at id#: ', doc.id);
-      return res.status(200).send(doc);
-      })
-    .catch(err => {
-    console.error(err);
-    return res.status(404).send({
-      error: 'Something went wrong. Unable to store new case data.',
-      err
-      });
+async function userExistsinFirestore(teleUser: string) {
+    const caseData = await firestoreCollection.where('telegramhandle', '==', teleUser).get();
+    return !caseData.empty;
+}
+
+function sendAlreadyRegisteredMessage(userId: number) {
+    const message = "Thanks for reaching out.\n\nIf you have any other issues you would like to raise, please do so through your befriender. If you feel uncomfortable doing so, you may leave a message with @transbefrienders.\n\nThank you and have a nice day!";
+    sendMessageToUserId(message, userId);
+}
+
+function sendNewRegistrationMessage(userId: number, username: string) {
+    const message = `Hello ${username}!\n\nThank you for reaching out to TransBefrienders' peer support service. We use this telegram bot to process your request, and to give you updates on your case should there be a need to do so.\n\nPlease fill up this form to the best of your ability. This is for us to know you better and a case manager will contact you after your application is processed. Any information you have submit through the form will be kept strictly confidential.\n\n${NEW_CASE_FORM_URL}\n\nYou will receive a confirmation message after you submit the form.`;
+    sendMessageToUserId(message, userId);
+}
+
+function sendMessageToUserId(message: string, userId: number) {
+    const options = {
+        chat_id: userId,
+        text: message
+    }
+    axios.post(telegramSendURL, options)
+    .then(function (response: any) {
+      console.log(response);
+    })
+    .catch(function (error: any) {
+      console.error(error);
     });
-});
-
-function buildNewCaseMessage(jsonData: any) {
-  
 }
--- user/src/newCaseHello.ts
+++ user/src/newCaseHello.ts
@@ -4,20 +4,55 @@ import axios from 'axios';
 
 const PROJ_ID = process.env.PROJ_ID;
 const COLLECTION_NAME: string = process.env.COLLECTION_NAME ?? 'undefined';
-const firestoreCollection = new firestore.Firestore({
+const firestoreCollection = new firestore.Firestore({// put up here because of how functions are instantiated. improves response times.
     projectId: PROJ_ID
 }).collection(COLLECTION_NAME);
+const TELEGRAM_URL = process.env.TELEGRAM_URL ?? "";
+const telegramSendURL = TELEGRAM_URL + (process.env.TELEGRAM_BOT_KEY ?? "") + "/sendMessage";
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+const NEW_CASE_FORM_URL = process.env.NEW_CASE_FORM_URL;
 
-functions.http('newCaseHello', (req: functions.Request, res: functions.Response) => {
-
-    
-    if(true){//telegram user id exists in newcasedata store
-
+functions.http('newCaseHello', async (req: functions.Request, res: functions.Response) => {
+    const updateObject = req.body;
+    const teleUser = '@' + updateObject.message.chat.username;
+    const teleUserId = updateObject.message.chat.id;
+    if(await userExistsinFirestore(teleUser)) {
+        sendAlreadyRegisteredMessage(teleUserId);
+        res.sendStatus(200);
     }
+    else {
+        sendNewRegistrationMessage(teleUserId, teleUser);
+        res.sendStatus(200);
+    }
+
 })
 
-async function doesUserExistinFirestore(userID) {
+async function userExistsinFirestore(teleUser: string) {
 
-    const caseData = await firestoreCollection.where(COLLECTION_NAME, '==', userID).get();
+    const caseData = await firestoreCollection.where('telegramhandle', '==', teleUser).get();
+    return caseData.empty;
+}
 
+function sendAlreadyRegisteredMessage(userId: number) {
+    const message = "Thanks for reaching out.\n\nIf you have any other issues you would like to raise, please do so through your befriender. If you feel uncomfortable doing so, you may leave a message with @transbefrienders.\n\nThank you and have a nice day!";
+    sendMessageToUserId(message, userId);
+}
+
+function sendNewRegistrationMessage(userId: number, username: string) {
+    const message = `Hello ${username}!\n\nThank you for reaching out to TransBefrienders' peer support service. We use this telegram bot to process your request, and to give you updates on your case should there be a need to do so.\n\nPlease fill up this form to the best of your ability. This is for us to know you better and a case manager will contact you after your application is processed. Any information you have submit through the form will be kept strictly confidential.\n\n${NEW_CASE_FORM_URL}\n\nYou will receive a confirmation message after you submit the form.`;
+    sendMessageToUserId(message, userId);
+}
+
+function sendMessageToUserId(message: string, userId: number) {
+    const options = {
+        chat_id: userId,
+        text: message
+    }
+    axios.post(telegramSendURL, options)
+    .then(function (response: any) {
+      console.log(response);
+    })
+    .catch(function (error: any) {
+      console.error(error);
+    });
 }
